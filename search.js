76c305b5aaccc0bc4b5d3f77cedf0e009b352187
Ben Regenspan <bregenspan@gmail.com>
2015-03-15 21:13:15 -0400

    Working request to TESS

--- search.js
+++ search.js
@@ -9,39 +9,56 @@ var HOST = 'http://tmsearch.uspto.gov';
 var HANDLER_URL = HOST + '/bin/gate.exe';
 var LOGIN = HANDLER_URL + '?f=login&p_lang=english&p_d=trmk';
 
-rp = rp.defaults({ jar: true });
+var jar = rp.jar();
+
+rp = rp.defaults({
+	jar: jar,
+	proxy: 'http://localhost:8888',
+	headers: {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0 (compatible; TESS-GIF)'
+	}
+});
 
 rp(LOGIN)
 	.catch(console.error)
 	.then(function (data) {
+        // Get a link to the search page (which includes session token)
 		var $ = cheerio.load(data),
 			link = $('a:contains("Free Form")');
         if (link.length) {
             return HOST + link.attr('href');
         } else {
-            throw 'Failed to find link to free-form search page!' + data;
+            throw 'Failed to find link to search page!' + data;
         }
     })
     .then(function (link) {
-        console.log(link);
-        return rp(link);
+        // Load the search page
+        return rp(link, { resolveWithFullResponse: true });
     })
     .then(function (response) {
-        var $ = cheerio.load(response);
+        // Search
+        var $ = cheerio.load(response.body);
         console.log(response);
         var formData = $('form[name=search_text]').serializeArray().reduce(function (o, v) {
             o[v.name] = v.value;
             return o;
         }, {});
         formData.p_s_ALL = '100102[dc] AND 030926[dc]';
-        formData.p_lang = 'english';
-        formData.a_search = 'Submit Query';
-        console.log(formData);
+		formData.a_search = 'Submit Query';
+		//jar.setCookie(rp.cookie('queryCookie=' + encodeURIComponent(formData.p_s_ALL)), HOST);
+
         return rp.post({
             uri: HANDLER_URL,
-            formData: formData
+            form: formData,
+			resolveWithFullResponse: true,
+			headers: {
+				Referer: response.request.href
+			}
         });
 	})
     .then(function (response) {
-        console.log('EWAPOAW' + response + 'ASDASD');
+		console.log(response);
+		console.log(response.req.headers);
+
+        console.log('EWAPOAW' + response.body + 'ASDASD');
     });
