704e4c859f01924e3bc2afd1f3f4646e9db27f68
Ben Regenspan <bregenspan@gmail.com>
2015-03-16 21:09:02 -0400

    Find and log out links to logos featuring animals smoking

--- search.js
+++ search.js
@@ -8,12 +8,15 @@ var cheerio = require('cheerio');
 var HOST = 'http://tmsearch.uspto.gov';
 var HANDLER_URL = HOST + '/bin/gate.exe';
 var LOGIN = HANDLER_URL + '?f=login&p_lang=english&p_d=trmk';
+var SERIAL_ROW_NUMBER = 2;
+var DOCUMENT_LINK_SELECTOR = 'a[href*="f=doc"]';
+var IMAGE_HANDLER_URL = HOST + '/ImageAgent/ImageAgentProxy?getImage={serialNumber}&widthLimit=400&heightLimit=300';
 
 var jar = rp.jar();
 
 rp = rp.defaults({
 	jar: jar,
-	proxy: 'http://localhost:8888',
+	//proxy: 'http://localhost:8888',
 	headers: {
 		'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0 (compatible; TESS-GIF)'
 	}
@@ -43,8 +46,9 @@ rp(LOGIN)
             o[v.name] = v.value;
             return o;
         }, {});
-        formData.p_s_ALL = '100102[dc] AND 030926[dc]';
+        formData.p_s_ALL = '100102[dc] and 03$[dc]';
 		formData.a_search = 'Submit Query';
+		formData.p_L = 500;  // 500 results/page
 		//jar.setCookie(rp.cookie('queryCookie=' + encodeURIComponent(formData.p_s_ALL)), HOST);
 
         return rp.post({
@@ -57,8 +61,21 @@ rp(LOGIN)
         });
 	})
     .then(function (response) {
+		var $ = cheerio.load(response.body);
+		var docs = $('a[href*="f=doc"]');
+
 		console.log(response);
 		console.log(response.req.headers);
 
-        console.log('EWAPOAW' + response.body + 'ASDASD');
+		// get this link for page of images:
+		//$('a[href*="brws_img');
+		
+		// Print serial numbers
+		$('td:nth-of-type(' + SERIAL_ROW_NUMBER + ') ' + DOCUMENT_LINK_SELECTOR).each(function (i, el) {
+			console.log(IMAGE_HANDLER_URL.replace('{serialNumber}', $(el).html()) + '\n');
+		});
+
+		// or just hit this for each as needed:
+		// <img src="/ImageAgent/ImageAgentProxy?getImage=86445370&widthLimit=400&heightLimit=300"  border="0" alt="Mark Image" 		// or just hit 
+        //console.log('EWAPOAW' + response.body + 'ASDASD');
     });
